e95b4eb4af630bcd38d1c1ec25cda9db71dac025
Justin Unger <justin_unger@apple.com>
2017-04-27 15:58:11 -0700

    try to add some error handling bubble up

--- ps2/src/main.rs
+++ ps2/src/main.rs
@@ -15,17 +15,19 @@ use getopts::Options;
 use std::env;
 use std::io::{self, Write};
 use std::process::Command;
+use std::error::Error;
 
 struct Shell<'a> {
     cmd_prompt: &'a str,
+    cmd_history: Vec<String>
 }
 
 impl <'a>Shell<'a> {
     fn new(prompt_str: &'a str) -> Shell<'a> {
-        Shell { cmd_prompt: prompt_str }
+        Shell { cmd_prompt: prompt_str, cmd_history: vec![] }
     }
 
-    fn run(&self) {
+    fn run(&mut self) {
         let stdin = io::stdin();
         let mut stdout = io::stdout();
 
@@ -37,17 +39,45 @@ impl <'a>Shell<'a> {
 
             stdin.read_line(&mut line).unwrap();
             let cmd_line = line.trim();
-            let program = cmd_line.splitn(1, ' ').nth(0).expect("no program");
-
-            match program {
-                ""      =>  { continue; }
-                "exit"  =>  { return; }
-                _       =>  { self.run_cmdline(cmd_line); }
-            }
+            match self.process_cmdline(cmd_line) {
+                Ok(_) => continue,
+                Err(e) => io::stdout().write(e.description().as_bytes()).unwrap()
+            };
         }
     }
 
-    fn run_cmdline(&self, cmd_line: &str) {
+    fn process_cmdline(&mut self, cmd_line: &str) -> Result<(),io::Error> {
+        let program = cmd_line.splitn(2, ' ').nth(0).expect("no program");
+        let cmd_line_chars: Vec<char> = cmd_line.chars().collect();
+        let len = cmd_line_chars.len();
+        let background = if len == 0 { false } else { cmd_line_chars[len-1] == '&'};
+
+        if program != "" { 
+            self.cmd_history.push(String::from(cmd_line));
+        }
+
+        match program {
+            ""      =>  { Ok(()) }
+            "exit"  =>  { Ok(()) }
+            "cd"    =>  { self.run_cd(cmd_line) }
+            "history" => { Ok(self.run_history()) }
+            _       =>  { self.run_cmdline(cmd_line,background) }
+        }
+    }
+
+    fn run_history(&self) {
+        for (i,cmd) in self.cmd_history.iter().enumerate() {
+            io::stdout().write(format!("  {}   {}\n",i,cmd).as_bytes()).unwrap();
+        }
+    }
+
+    fn run_cd(&self, cmd_line: &str) -> Result<(),io::Error> {
+        let argv: Vec<&str> = cmd_line.split_whitespace().collect();
+        env::set_current_dir(&argv[1])?;
+        Ok(())
+    }
+
+    fn run_cmdline(&self, cmd_line: &str, background: bool) -> Result<(),io::Error> {
         let argv: Vec<&str> = cmd_line.split(' ').filter_map(|x| {
             if x == "" {
                 None
@@ -57,16 +87,25 @@ impl <'a>Shell<'a> {
         }).collect();
 
         match argv.first() {
-            Some(&program) => self.run_cmd(program, &argv[1..]),
-            None => (),
-        };
+            Some(&program) => self.run_cmd(program, &argv[1..], background),
+            None => Ok(())
+        }
     }
 
-    fn run_cmd(&self, program: &str, argv: &[&str]) {
+    fn run_cmd(&self, program: &str, argv: &[&str], background: bool) -> Result<(),io::Error> {
         if self.cmd_exists(program) {
-            io::stdout().write(&Command::new(program).args(argv).output().unwrap().stdout).unwrap();
+            if !background {
+                io::stdout().write(&Command::new(program).args(argv).output().unwrap().stdout)?;
+                Ok(())
+            } else {
+                let mut c = Command::new(program);
+                let c = c.args(argv);
+                let child = c.spawn();
+                Ok(())
+            }
         } else {
             println!("{}: command not found", program);
+            Ok(())
         }
     }
 
@@ -89,7 +128,8 @@ fn main() {
     let opt_cmd_line = get_cmdline_from_args();
 
     match opt_cmd_line {
-        Some(cmd_line) => Shell::new("").run_cmdline(&cmd_line),
+        //Some(cmd_line) => Shell::new("").run_cmdline(&cmd_line, false),
+        Some(_)        => Shell::new("gash > ").run(),
         None           => Shell::new("gash > ").run(),
     }
 }
